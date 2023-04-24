6033605e6d95e568e917b20550ac5065d2844c93
cspup <yemuc@qq.com>
2023-04-24 15:29:47 +0800

    init

---
+++ src/main/java/com/cspup/notespro/NotesProApplication.java
@@ -0,0 +1,13 @@
+package com.cspup.notespro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cspup/notespro/NotesProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cspup.notespro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
